fff7ae37864123023792b7da46e8587bda534f3a
jokersk <jokersk119@gmail.com>
2022-10-07 17:59:16 +0800

    wip

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -4,10 +4,10 @@
             <div class="text-center">
                 <h2 class="text-lg font-semibold text-indigo-600">Diff</h2>
                 <p class="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
-                    Easy find the text differece
+                    Easy find the text difference
                 </p>
                 <p class="mx-auto mt-5 max-w-xl text-xl text-gray-500">
-                    For free, easy find the text differece include html!
+                    For free, easy find the text difference include html!
                 </p>
             </div>
         </div>
